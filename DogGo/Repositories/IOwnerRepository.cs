e22b2f5f07e7eae3e855c2544e74c13244062ded
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-03-30 06:52:05 -0500

    In lesson 3

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+        void AddOwner(Owner owner);
+        void UpdateOwner(Owner owner);
+        void DeleteOwner(int ownerId);
+    }
+}
